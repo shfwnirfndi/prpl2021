4b28d0c1364cd4c7ab4bbf729602b69bc4d0350a
shfwnirfndi <shafwanirfand261101@gmail.com>
2021-07-27 21:32:40 +0700

    commit

--- kalkulator.php
+++ kalkulator.php
@@ -1,87 +1,100 @@
 <?php
+interface KalkulatorBangunRuang{
+    public function hitungBangunRuang($satuan);
 
-interface pilihanKalkulatorBangunRuang{
-    public function kalku();
 }
-
-class LuasPersegiPanjang implements pilihanKalkulatorBangunRuang{
+class Persegi_Panjang implements KalkulatorBangunRuang {
     public $panjang;
     public $lebar;
 
-    public function kalku(){
-        return $this->panjang * $this->lebar;
+    public function hitungBangunRuang($satuan) {
+        $this->panjang = $satuan['panjang'];
+        $this->lebar = $satuan['lebar'];
+        return ('Bangun Ruang : Persegi Panjang<br>'.
+                'Panjang : '.$this->panjang.' m<br>'.
+                'Lebar : '.$this->lebar.' m<br>'.
+                'Rumus : panjang x lebar<br>'.
+                'Luas Persegi_Panjang : '.$this->panjang * $this->lebar.' m2');
     }
 }
-
-class VolumeBola implements pilihanKalkulatorBangunRuang{
-    public $jari;
-    public $phi = 3.14;
-    public function kalku(){
-        return (4/3) * $this->phi * $this->jari * $this->jari;
-    }
-}
-
-class VolumeKerucut implements pilihanKalkulatorBangunRuang{
-    public $tinggi;
-    public $jari;
-    public $phi = 13.4;
-
-    public function kalku(){
-        return (1/3) * $this->phi * $this->jari * $this->jari * $this->jari * $this->tinggi;
-    }
-}
-
-class VolumeKubus implements pilihanKalkulatorBangunRuang{
+class Kubus implements KalkulatorBangunRuang {
     public $rusuk;
 
-    public function kalku(){
-        return $this->rusuk * $this->rusuk * $this->rusuk; 
+    public function hitungBangunRuang($satuan) {
+        $this->rusuk = $satuan['rusuk'];
+        return ('Bangun Ruang : Kubus<br>'.
+                'Panjang Rusuk (r) : '.$this->rusuk.' m<br>'.
+                'rumus : r x r x r<br>'.
+                'Volume Kubus diperoleh hasil : '.$this->rusuk * $this->rusuk * $this->rusuk.' m3');
+    }
+}
+class Lingkaran implements KalkulatorBangunRuang {
+    public $jejari;
+    public function hitungBangunRuang($satuan) {
+        $this->jejari = $satuan['jejari'];
+        return ('Bangun Ruang : Lingkaran<br>'.
+                'Jejari (r) : '.$this->jejari.' m<br>'.
+                'hitung : 2 x phi x r<br>'.
+                'Keliling Lingkaran diperoleh hasil: '.(2 * pi() * $this->jejari.' m'));
+    }
+}
+class Kerucut implements KalkulatorBangunRuang {
+    public $tinggi;
+    public $jejari;
+
+    public function hitungBangunRuang($satuan) {
+        $this->tinggi = $satuan['tinggi'];
+        $this->jejari = $satuan['jejari'];
+        return ('Bangun Ruang : Kerucut<br>'.
+                'Tinggi : '.$this->tinggi.' m<br>'.
+                'Jejari (r) : '.$this->jejari.' m<br>'.
+                'hitung : 1/3 x luas alas (lingkaran: phi*r*r) x t<br>'.
+                'Volume Kerucut diperoleh hasil : '.(1/3) * pi() * $this->jejari * $this->jejari * $this->tinggi.' m3');
+    }
+}
+class Bola implements KalkulatorBangunRuang {
+    public $jejari;
+
+    public function hitungBangunRuang($satuan) {
+        $this->jejari = $satuan['jejari'];
+        return ('Bangun Ruang : Bola<br>'.
+                'Jejari (r) : '.$this->jejari.' m<br>'.
+                'Rumus : 4/3 x phi x r x r x r<br>'.
+                'Volume Bola : '.(4/3) * pi() * $this->jejari * $this->jejari * $this->jejari.' m3');
     }
 }
 
-class KelilingLingkaran implements pilihanKalkulatorBangunRuang{
-    public $jari;
-    public $phi = 13.4;
 
-    public function kalku(){
-        return 2 * $this->phi * $this->jari;
+class KalkulatorBangunRuangFactory{
+    public function initializeKalkulatorBangunRuang($pilih){
+        if($pilih=='luasPersegi_panjang'){
+            return new Persegi_panjang();
+        }
+        if($pilih=='Volume_bola'){
+            return new Bola();
+        }
+        if($pilih=='Volume_kerucut'){
+            return new Kerucut();
+        }
+        if($pilih=='Volume_kubus'){
+            return new Kubus();
+        }
+        if($pilih=='keliling_lingkaran'){
+            return new Lingkaran();
+        }
+    }
+}
+$satuan = ['rusuk'=>12, 'panjang'=>0, 'lebar'=>0, 'jejari'=>0, 'tinggi'=>0];
+class Json{
+    public static function form($data){
+        return json_encode($data);
     }
 }
 
-class JsonFile implements pilihanKalkulatorBangunRuang{
-    public $pilihanKalkulatorBangunRuang;
-    public function __kalku(string $pilihanKalkulatorBangunRuang){
-        $this->pilihanKalkulatorBangunRuang = $pilihanKalkulatorBangunRuang;
-    }
-}
-
-class kalkulatorBangunRuangFactory{
-    public function hitung($tipe){
-        if($tipe==='luaspersegipanjang'){
-            return LuasPersegiPanjang();
-        }
-        if($tipe==='volumebola'){
-            return VolumeBola();
-        }
-        if($tipe==='volumekerucut'){
-            return VolumeKerucut();
-        }
-        if($tipe==='volumekubus'){
-            return VolumeKubus();
-        }
-        if($tipe==='kelilinglingkaran'){
-            return KelilingLingkaran();
-        }
-        throw new Exception("Error!!!");
-    }
-}
-
-Route::get('/', function(){
-    $satuan = ['rusuk'=>12, 'tinggi'=>0, 'panjang'=>0, 'lebar'=>0, 'jari'=>0];
-    $pilihanKalkulatorBangunRuang = 'volumekubus';
-    $kalkulatorBangunRuangFactory = new kalkulatorBangunRuangFactory();
-    $kalkulatorBangunRuang = $kalkulatorBangunRuangFactory ->initializeKalkulatorBangunRuang($pilihanKalkulatorBangunRuang, $satuan);
-    $hasilKalkulatorBangunRuang = $kalkulatorBangunRuang ->hitung();
-    print_r($hasilKalkulatorBangunRuang);
-})
-?>
+print(Json::form($satuan));
+echo'<br><br>';
+$pilihanKalkulatorBangunRuang='Volume_kubus';
+$kalkulatorBangunRuangFactory = new KalkulatorBangunRuangFactory();
+$kalkulatorBangunRuang = $kalkulatorBangunRuangFactory->initializeKalkulatorBangunRuang($pilihanKalkulatorBangunRuang);
+$hasilKalkulatorBangunRuang = $kalkulatorBangunRuang->hitungBangunRuang($satuan);
+print_r($hasilKalkulatorBangunRuang);
